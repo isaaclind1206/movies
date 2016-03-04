f6026646e9c106e0c21ff2e7fa8f8d8f98dce08f
isaaclind1206 <isaac@madisonavenueworldwide.com>
2016-03-04 08:11:21 -0600

    Update fill_in_blanks.py

--- fill_in_blanks.py
+++ fill_in_blanks.py
@@ -1,11 +1,11 @@
 print "This is the Fill in the Blanks Python Quiz!"
 print "Good Luck!"
 
-num_of_blanks = ["blank1", "blank2", "blank3", "blank4"]
+num_of_blanks = ["____1____", "____2____", "____3____", "____4_____"]
 
 #These are the levels for the quiz
 easy_question= """ Bob ____1____ wrote Blowing in the Wind; He was friends John Lennon of The ____2____. He also recorded a song with Johnny ____3____ who sang Folsom Prison Blues. He also sang the song Like a Rolling___4____."""
-medium_question= """Romeo ____2_____ fell in love with _____2_____ Capulet. Her cousin _____3_____ killed Romeo's friend _____4______."""
+medium_question= """Romeo ____1_____ fell in love with _____2_____ Capulet. Her cousin _____3_____ killed Romeo's friend _____4______."""
 hard_question= """Doubt thou the ____1____ are fire, Doubt that the ____2_____ doth move; Doubt _____3_____ to be a liar; But never doubt I ____4_____."""
 
 #These are the answers
@@ -13,29 +13,20 @@ easy_answers= ["Dylan", "Lennon", "Cash", "Stone"]
 medium_answers= ["Montague", "Juliet", "Tybalt", "Mercutio"]
 hard_answers= ["Stars", "Sun", "Truth", "Love"]
 
+question, answers = question_level()
 #This will  deter the level of difficulty for each question
 def question_level():
     difficulty = raw_input("Choose the difficulty of your quiz!")
     if difficulty == "easy":
-        return easy_question
+        return easy_question, easy_answers
     elif difficulty == "medium":
-        return medium_question 
-    elif difficulty == "hard"
-        return hard_question
+        return medium_question, medium_answers 
+    elif difficulty == "hard":
+        return hard_question, hard_answers
     else:
         print "That is not a level."
 print question_level()
 
-def answer_level():
-    if difficulty == "easy":
-        answers = easy_answers
-        return = answers
-    if difficulty == "medium":
-        answers = medium_answers
-        return answers
-    else:
-        answers = hard_answers
-        return answers
 
 #This will allow user to take the quiz
 def take_quiz(num_of_blank):
@@ -51,3 +42,4 @@ def take_quiz(num_of_blank):
                     return "Please Try Again."
         print blank_string
         take_quiz()
+
